5b04079a779887c855cae38997431500f7422030
Zeeshan ali <zeeshanali90233@gmail.com>
2024-06-18 21:53:14 +0500

    Package Changes

--- callbacks/ResetUserPassword_Callback.js
+++ callbacks/ResetUserPassword_Callback.js
@@ -1,17 +1,19 @@
 import admin from "../lib/firebase.js";
 
 async function ResetUserPassword_Callback(req, res) {
-  const { userId } = req.body;
+  const { userId, newPassword } = req.body;
   if (!userId) {
     return res.status(404).json({ message: "User Id is missing" });
   }
   try {
-    await admin
-      .auth()
-      .updateUser(userId, { password: process.env.DEFAULT_PASSWORD });
+    await admin.auth().updateUser(userId, {
+      password: newPassword || process.env.DEFAULT_PASSWORD,
+    });
     return res.status(200).json({
       status: "Ok",
-      message: `Password Reseted to default ` + process.env.DEFAULT_PASSWORD,
+      message:
+        `Password Reseted to default ` +
+        (newPassword || process.env.DEFAULT_PASSWORD),
     });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
