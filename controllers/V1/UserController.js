4d5ce6fab3c2336e9ce81d478325270d8b9b4e84
Zeeshan ali <zeeshanali90233@gmail.com>
2024-06-22 20:23:48 +0500

    Delete USER API

--- controllers/V1/UserController.js
+++ controllers/V1/UserController.js
@@ -2,6 +2,7 @@ import express from "express";
 import ResetUserPassword_Callback from "../../callbacks/ResetUserPassword_Callback.js";
 import CheckUserAuthenticity from "../../middleware/UserAuthenticity.js";
 import DeleteUser_Callback from "../../callbacks/DeleteUser_Callback.js";
+import DeleteUserPassword_Callback from "../../callbacks/DeleteUserPassword_Callback.js";
 
 const Router = express.Router();
 
@@ -10,6 +11,7 @@ Router.post(
   CheckUserAuthenticity,
   ResetUserPassword_Callback
 );
+Router.delete("/delete", CheckUserAuthenticity, DeleteUserPassword_Callback);
 Router.delete("/delete", CheckUserAuthenticity, DeleteUser_Callback);
 
 export default Router;
